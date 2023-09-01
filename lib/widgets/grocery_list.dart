abaaab28a606e843b80b72030d0c09f2957d7366
Thomas Wolfe <tjwolfe91@gmail.com>
2023-09-01 19:48:12 -0400

    Update grocery_list.dart

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
 import 'package:shopping_list/widgets/new_item.dart';
 import 'package:shopping_list/models/grocery_item.dart';
+import 'package:shopping_list/data/categories.dart';
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -11,22 +13,51 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
+
+  @override // initializing loadItems() when the app starts up with super.initState()
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+
+  void _loadItems() async {
+    final url = Uri.https(
+        'flutter-prep-95c73-default-rtdb.firebaseio.com', 'shopping-list.json');
+    final response = await http.get(url);
+    final Map<String, dynamic> listData = json.decode(response.body);
+    final List<GroceryItem> _loadedItems = [];
+
+    // convert json Map data to a dart List data
+    // creates a new grocery item, adds it to temporary loadeditems list, for every item part of the response.
+    //   So for every response item we create a new grocery item witch category of pre-defined Categories, AND the data from the back-end like unique ID provided by Firebase
+
+    for (final item in listData.entries) {
+      final category = categories.entries
+          .firstWhere(
+              (catItem) => catItem.value.title == item.value['category'])
+          .value; // matching category where a condition is met
+      _loadedItems.add(GroceryItem(
+        id: item.key,
+        name: item.value['name'],
+        quantity: item.value['quantity'],
+        category: category,
+      ));
+    }
+    setState(() {
+      _groceryItems =
+          _loadedItems; // overriding initial list of data with GET data
+    });
+  }
 
   void _addItem() async {
-    final newItem = await Navigator.of(context).push<GroceryItem>(
+    await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: (ctx) => const NewItem(),
       ),
     );
 
-    if (newItem == null) {
-      return;
-    }
-
-    setState(() {
-      _groceryItems.add(newItem);
-    });
+    _loadItems();
   }
 
   void _removeItem(GroceryItem item) {
