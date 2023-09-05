9d4e60c2fe0f31707f68a550a67a638ea876419d
Thomas Wolfe <tjwolfe91@gmail.com>
2023-09-05 13:36:05 -0400

    Update new_item.dart

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
+import 'package:shopping_list/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -40,13 +41,22 @@ class _NewItemState extends State<NewItem> {
         ),
       );
 
-      print(response.body);
-      print(response.statusCode);
+      final Map<String, dynamic> resData = json.decode(response.body);
+
+      // print(response.body);
+      // print(response.statusCode);
 
       if (!context.mounted) {
         return;
       }
-      Navigator.of(context).pop();
+      Navigator.of(context).pop(
+        GroceryItem(
+          id: resData['name'],
+          name: _enteredName,
+          quantity: _enteredQuantity,
+          category: _selectedCategory,
+        ),
+      );
       //   .then((resopnse) {
       // work with that response
       //  });
